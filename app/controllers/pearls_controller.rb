fb49f41bd0a8f63513ff4dacaa66ac6001ab4837
unknown <baderm7@TROMWN2275.emea.roche.com>
2014-07-01 16:43:50 +0200

    ncer logo

--- app/controllers/pearls_controller.rb
+++ app/controllers/pearls_controller.rb
@@ -31,7 +31,7 @@ class PearlsController < ApplicationController
 
     respond_to do |format|
       if @pearl.save
-        format.html { redirect_to @pearl, notice: 'Pearl was successfully created.' }
+        format.html { redirect_to @pearl, notice: 'Perle wurde erfolgreich angelegt.' }
         format.json { render :show, status: :created, location: @pearl }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class PearlsController < ApplicationController
   def update
     respond_to do |format|
       if @pearl.update(pearl_params)
-        format.html { redirect_to @pearl, notice: 'Pearl was successfully updated.' }
+        format.html { redirect_to @pearl, notice: 'Perle wurde erfolgreich bearbeitet.' }
         format.json { render :show, status: :ok, location: @pearl }
       else
         format.html { render :edit }
@@ -59,31 +59,32 @@ class PearlsController < ApplicationController
   def destroy
     @pearl.destroy
     respond_to do |format|
-      format.html { redirect_to pearls_url, notice: 'Pearl was successfully destroyed.' }
+      format.html { redirect_to pearls_url, notice: 'Perle wurde erfolgreich gelöscht.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pearl
-      @pearl = Pearl.friendly.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pearl_params
-      params.require(:pearl).permit(:user_id, :body, :title, :image, :remove_image)
-    end
-    
-    def make_sure_is_owner
-      @pearl = Pearl.friendly.find(params[:id])
-      if (user_signed_in? && current_user.id == @pearl.user.id)
-        
-      else        
-        respond_to do |format|      
-          format.html { redirect_to root_url, notice: 'you can only edit your own ideas.' }
-          format.json { head :no_content }          
-        end         
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pearl
+    @pearl = Pearl.friendly.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def pearl_params
+    params.require(:pearl).permit(:user_id, :body, :title, :image, :remove_image)
+  end
+
+  def make_sure_is_owner
+    @pearl = Pearl.friendly.find(params[:id])
+    if (user_signed_in? && current_user.id == @pearl.user.id)
+
+      else
+      respond_to do |format|
+        format.html { redirect_to pearl_path(@pearl), alert: 'Sie können nur ihre eigenen Perlen bearbeiten!' }
+        format.json { head :no_content }
       end
     end
+  end
 end
--- app/views/pearls/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@pearls) do |pearl|
-  json.extract! pearl, :id, :user_id, :body, :title, :image
-  json.url pearl_url(pearl, format: :json)
-end
