bdd155913800e6be3eb3d60a96139b5273fa19cf
unknown <baderm7@TROMWN2275.emea.roche.com>
2014-07-04 16:11:53 +0200

    forum

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
  def needs_sidepanel?
-   current_page?(root_path) || current_page?(who_am_i_path) || current_page?(what_do_i_offer_path) || current_page?(contact_path) || current_page?(pages_contacted_path) 
+   current_page?(root_path) || current_page?(who_am_i_path) || current_page?(what_do_i_offer_path) || current_page?(pages_contacted_path) 
  end
 
 end
