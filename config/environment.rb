0ff31e6ea8fad3e3cd67428111395ba66bac2d91
sastrian <maro.bader@gmail.com>
2014-07-12 09:50:13 +0200

    email settings

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,13 @@ load(app_env_vars) if File.exists?(app_env_vars)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'http://serene-fortress-3748.herokuapp.com/',
+  :user_name            => ENV['EMAIL'],
+  :password             => ENV['PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
