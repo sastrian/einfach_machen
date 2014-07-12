785c16c042c62c372e4d62a71a72627fe20c5f74
sastrian <maro.bader@gmail.com>
2014-07-12 09:33:42 +0200

    devise config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.action_mailer.default_url_options = { :host => 'serene-fortress-3748.herokuapp.com' }
+
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
